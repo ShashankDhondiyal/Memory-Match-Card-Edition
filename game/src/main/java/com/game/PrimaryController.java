237b28074f2991f6312e068b75c50ddc39825a68
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-03-21 21:03:50 -0400

    v1.0.0
    Added JavaFX Project files and updated pom configurations. Modified  FXML file.

---
+++ game/src/main/java/com/game/PrimaryController.java
@@ -0,0 +1,13 @@
+package com.game;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void startGame() throws IOException {
+        // WIP
+    }
+}
