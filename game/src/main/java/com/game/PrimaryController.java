421e5241accb6721c96d8c5147e8ca61c7e40f21
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-03-21 21:31:55 -0400

    v1.0.2
    Added action in fxml and PrimaryController for clicking images in the game grid

--- game/src/main/java/com/game/PrimaryController.java
+++ game/src/main/java/com/game/PrimaryController.java
@@ -2,6 +2,7 @@ package com.game;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class PrimaryController {
 
@@ -31,6 +32,71 @@ public class PrimaryController {
 
         for (int i = 0; i < 16; i++) {
             gameGridCards[i].setImage(deck.getCard(53));
+        }
+
+        // maybe make other java for making the game display other cards
+
+    }
+
+    @FXML
+    public void handleClick(MouseEvent event) {
+        Object source = event.getSource();
+
+        if (source instanceof ImageView) {
+            ImageView clickedImageView = (ImageView) source;
+            
+            String imageViewId = clickedImageView.getId();
+            
+            switch (imageViewId){
+                case "card1":
+                    // add logic 
+                    break;
+                case "card2":
+                    // add logic 
+                    break;
+                case "card3":
+                    // add logic 
+                    break;
+                case "card4":
+                    // add logic 
+                    break;
+                case "card5":
+                    // add logic 
+                    break;
+                case "card6":
+                    // add logic 
+                    break;
+                case "card7":
+                    // add logic 
+                    break;
+                case "card8":
+                    // add logic 
+                    break;
+                case "card9":
+                    // add logic 
+                    break;
+                case "card10":
+                    // add logic 
+                    break;
+                case "card11":
+                    // add logic 
+                    break;
+                case "card12":
+                    // add logic 
+                    break;
+                case "card13":
+                    // add logic 
+                    break;
+                case "card14":
+                    // add logic 
+                    break;
+                case "card15":
+                    // add logic 
+                    break;
+                case "card16":
+                    // add logic 
+                    break;
+            }           
         }
     }
 }
